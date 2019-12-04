448ee35612453d177f4a6574ff06c5063093bbae
Wiktor Kubasiak <56201715+wiktor-kubasiak@users.noreply.github.com>
2019-12-04 16:17:57 +0000

    Update HelloPython.py

--- HelloPython.py
+++ HelloPython.py
@@ -1,9 +1,13 @@
-from flask import Flask
 import datetime
+from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-print("Hello World!")
-print("Hello Wiktor!")
-now = datetime.datetime.now()
-print("Current date and time is ")
-print(now.strftime("%A, %d-%m-%Y : %H:%M"))
+def index():
+    print("Hello World!")
+    print("Hello Wiktor!")
+    now = datetime.datetime.now()
+    print("Current date and time is ")
+    print(now.strftime("%A, %d-%m-%Y : %H:%M"))
+    return "Hello!"
+  
+index()
